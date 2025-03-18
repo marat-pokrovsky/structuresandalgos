2ed61e1872101a33e7bc427aaab0800d9b65a549
marat-pokrovsky <175550684+marat-pokrovsky@users.noreply.github.com>
2025-03-18 00:00:00 +0000

    Implement append method for linked list

---
+++ data_structures/linked_list/linked_list.py
@@ -0,0 +1,15 @@
+from .node import Node
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
