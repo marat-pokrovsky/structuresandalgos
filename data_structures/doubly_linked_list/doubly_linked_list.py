01df8271f1a2c911906a900f8c7763daf7279264
marat-pokrovsky <175550684+marat-pokrovsky@users.noreply.github.com>
2025-03-30 00:00:00 +0000

    feat: Implement DoublyLinkedList

---
+++ data_structures/doubly_linked_list/doubly_linked_list.py
@@ -0,0 +1,31 @@
+from .node import Node
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+        new_node.prev = last_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+        if self.head is not None:
+            self.head.prev = new_node
+        new_node.next = self.head
+        self.head = new_node
+
+    def to_list(self):
+        nodes = []
+        current_node = self.head
+        while current_node:
+            nodes.append(current_node.data)
+            current_node = current_node.next
+        return nodes
