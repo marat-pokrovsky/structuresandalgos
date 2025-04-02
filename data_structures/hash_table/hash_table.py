d5fbc3b47bb027ad3395ce8af56ad052d7f92f0b
marat-pokrovsky <175550684+marat-pokrovsky@users.noreply.github.com>
2025-04-02 00:00:00 +0000

    feat: Implement HashTable

---
+++ data_structures/hash_table/hash_table.py
@@ -0,0 +1,39 @@
+class HashTable:
+    def __init__(self, size = 10):
+        self.size = size
+        self.table = [[] for _ in range(self.size)]
+
+    def _hash(self, key):
+        return hash(key) % self.size
+
+    def set(self, key, value):
+        key_hash = self._hash(key)
+        key_value = [key, value]
+
+        if self.table[key_hash] is None:
+            self.table[key_hash] = list([key_value])
+            return True
+        else:
+            for pair in self.table[key_hash]:
+                if pair[0] == key:
+                    pair[1] = value
+                    return True
+            self.table[key_hash].append(key_value)
+            return True
+
+    def get(self, key):
+        key_hash = self._hash(key)
+        if self.table[key_hash] is not None:
+            for pair in self.table[key_hash]:
+                if pair[0] == key:
+                    return pair[1]
+        return None
+
+    def delete(self, key):
+      key_hash = self._hash(key)
+      if self.table[key_hash] is not None:
+        for i in range(len(self.table[key_hash])):
+          if self.table[key_hash][i][0] == key:
+            self.table[key_hash].pop(i)
+            return True
+      return False
