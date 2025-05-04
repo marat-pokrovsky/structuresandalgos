a41a84e48b450529ac5bcb268d9537de88a636ea
marat-pokrovsky <175550684+marat-pokrovsky@users.noreply.github.com>
2025-05-04 00:00:00 +0000

    feat: add docstring

--- data_structures/doubly_linked_list/doubly_linked_list.py
+++ data_structures/doubly_linked_list/doubly_linked_list.py
@@ -1,10 +1,14 @@
 from .node import Node
 
 class DoublyLinkedList:
+    """A doubly linked list implementation."""
+
     def __init__(self):
+        """Initializes an empty doubly linked list."""
         self.head = None
 
     def append(self, data):
+        """Appends a new node with the given data to the end of the list."""
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
@@ -16,6 +20,7 @@ class DoublyLinkedList:
         new_node.prev = last_node
 
     def prepend(self, data):
+        """Prepends a new node with the given data to the beginning of the list."""
         new_node = Node(data)
         if self.head is not None:
             self.head.prev = new_node
@@ -23,6 +28,7 @@ class DoublyLinkedList:
         self.head = new_node
 
     def to_list(self):
+        """Converts the doubly linked list to a Python list."""
         nodes = []
         current_node = self.head
         while current_node:
