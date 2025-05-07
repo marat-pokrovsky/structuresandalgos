8332049c44af744a89bf8e45c102cb0480ccc1fb
marat-pokrovsky <175550684+marat-pokrovsky@users.noreply.github.com>
2025-05-07 00:00:00 +0000

    feat: add docstring

--- data_structures/linked_list/linked_list.py
+++ data_structures/linked_list/linked_list.py
@@ -1,10 +1,14 @@
 from .node import Node
 
 class LinkedList:
+    """A singly linked list implementation."""
+
     def __init__(self):
+        """Initializes an empty linked list."""
         self.head = None
 
     def append(self, data):
+        """Appends a new node with the given data to the end of the list."""
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
@@ -15,11 +19,13 @@ class LinkedList:
         last_node.next = new_node
 
     def prepend(self, data):
+        """Prepends a new node with the given data to the beginning of the list."""
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
 
     def delete(self, data):
+        """Deletes the first occurrence of a node with the given data."""
         current_node = self.head
 
         if current_node and current_node.data == data:
@@ -39,6 +45,7 @@ class LinkedList:
         current_node = None
 
     def insert_after(self, prev_node_data, data):
+        """Inserts a new node with the given data after the first occurrence of a node with the given previous node data."""
         current_node = self.head
         while current_node and current_node.data != prev_node_data:
             current_node = current_node.next
@@ -49,6 +56,7 @@ class LinkedList:
             current_node.next = new_node
 
     def to_list(self):
+        """Converts the linked list to a Python list."""
         nodes = []
         current_node = self.head
         while current_node:
@@ -57,6 +65,7 @@ class LinkedList:
         return nodes
 
     def __str__(self):
+        """Returns a string representation of the linked list."""
         nodes = []
         current_node = self.head
         while current_node:
